ae6c8251e61bc0a663c05fe3215ed0f59f62f2e7
Manel Martinez Gonzalez <manel@MacBook-de-Manel.local>
2015-04-06 22:01:48 +0200

    first commit

--- bin/prepare-ssh.sh
+++ bin/prepare-ssh.sh
@@ -6,7 +6,7 @@ set -e
 
 # Change root password
 if [ "${VPN_PASSWORD}" == "**ChangeMe**" ]; then
-   echo "ERROR: You did not specify "-e VPN_PASSWORD" environment variable - Exiting..."
-   exit 1
+   VPN_PASSWORD=`pwgen -s 20 1`
+   echo "If you are using nixel/rancher-vpn-client docker image you must define VPN_PASSWORD environment variable with -e \"VPN_PASSWORD=$VPN_PASSWORD\""
 fi
 echo "root:${VPN_PASSWORD}" | chpasswd
